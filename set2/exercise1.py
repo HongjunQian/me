85310aa2c7d09b7c2c2ec4c7855f10cbbed3eedb
HongjunQian <hongjunqian08@gmail.com>
2023-06-14 20:57:37 +1000

    Wee2 homework
    I got a bad cold since last weekends. Can my homework still be accepted?

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -18,26 +18,26 @@ import platform
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ["what", "does", "this", "line", "do", "?"]
-
+# I think this will print "word"
 for word in some_words:
-    print(word)
-
+    print(word) # it printed every value in the list one by one
+# I think this will print everything in the list
 for x in some_words:
-    print(x)
+    print(x)    # it printed every value in the list one by one
+# I think this will print every words in the list except the question mark
+print(some_words)   # it prints ['what', 'does', 'this', 'line', 'do', '?']
+# I think this will print some_words contains more than 3 words
+if len(some_words) > 3:     #it prints "some_words contains more than 3 words" because the length of (some_words)is greater than 3
+    print("some_words contains more than 3 words")  #it prints "some_words contains more than 3 words"
 
-print(some_words)
-
-if len(some_words) > 3:
-    print("some_words contains more than 3 words")
-
-
+# I think this will print a namedtuple containing six attributes
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname()) # it prints uname_result(system='Windows', node='MSI', release='10', version='10.0.19045', machine='AMD64')
 
 
 usefulFunction()
--- set2/readme.md
+++ set2/readme.md
@@ -1,1 +1,25 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+exercise1: List of programming volcabulary:
+    "call" =  do or execute a specific function
+    "return" = ends the executed function and get the result
+    "function" = a block of code that can be referenced by name to run the code it contains
+    "argument" = the information contained in the function
+    "syntax" = rules of programming language
+    "array" = a set of objects that are all the same type of data
+    "loop" =  a sequence of instruction that is continually repeated until a certain condition is reached
+
+exercise2: it prints <function set2exersise2 at 0x0000018396E7CEA0>
+
+Reflection:
+This week I learned how to do loops and print lists with my code. But I found myself unable to complete 
+exercise3 based on what I have learned from the lecture slides. I watched the answers vedio andntrying 
+hard to understand the logic behind it, and it's a bit painful.
+
+Question for Set2, exercise3, loops_1c: 
+
+I'm unclear about loops_1c because I have passed the test but the answer is not quite right.
+It prints:['×°×', '×°×', '×°×', '×°×'] instead of ['#', '#', '#', '#', '#']
+I have tried using str() to change the variables but it did not work.
+
+Finally. I apologize for the late submission. I got a bad cold since last weekend.
+I would be greatful if you can still accept my homework.
